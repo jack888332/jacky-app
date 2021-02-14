09a7a3e347938d44ed83fb1ea6d852405a89ebc8
jack888332 <Jacky@windows10.microdone.cn>
2021-02-15 02:55:14 +0800

    1

--- src/main/java/test/base/socket/ClientTCP.java
+++ src/main/java/test/base/socket/ClientTCP.java
@@ -5,10 +5,8 @@ import java.io.BufferedOutputStream;
 import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.InetSocketAddress;
 
