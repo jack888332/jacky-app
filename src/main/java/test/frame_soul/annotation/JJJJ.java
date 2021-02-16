ea510e9399e4bc42d4cfec6a594ce3c8204e4d9a
jack888332 <Jacky@windows10.microdone.cn>
2021-02-17 01:50:55 +0800

    1

---
+++ src/main/java/test/frame_soul/annotation/JJJJ.java
@@ -0,0 +1,10 @@
+package test.frame_soul.annotation;
+
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+import java.lang.annotation.Retention;
+
+@Retention(CLASS)
+public @interface JJJJ {
+
+}
---
+++ src/main/java/test/frame_soul/reflect/Person.java
@@ -0,0 +1,63 @@
+package test.frame_soul.reflect;
+
+public class Person {
+
+	public String name;
+	protected int age;
+	int height;
+	private int weight;
+
+	public Person() {
+		super();
+	}
+
+	public Person(String name, int age, int height, int weight) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setName(String firstName, String lastName) {
+		this.name = firstName + " " + lastName;
+	}
+
+	protected int getAge() {
+		return age;
+	}
+
+	protected void setAge(int age) {
+		this.age = age;
+	}
+
+	protected int getHeight() {
+		return height;
+	}
+
+	protected void setHeight(int height) {
+		this.height = height;
+	}
+
+	protected int getWeight() {
+		return weight;
+	}
+
+	protected void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + ", height=" + height + ", weight=" + weight + "]";
+	}
+
+}
