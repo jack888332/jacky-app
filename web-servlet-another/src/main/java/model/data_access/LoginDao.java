a1ed009ac32fbb94813df748cb5470957b66b631
jack888332 <jack888332@gmail.com>
2021-10-09 12:19:59 +0800

    111111

---
+++ web-servlet-another/src/main/java/model/data_access/LoginDao.java
@@ -0,0 +1,5 @@
+package model.data_access;
+
+public interface LoginDao {
+	boolean checkLoginForm();
+}
---
+++ web-servlet-another/src/main/java/model/data_access/LoginDaoImpl.java
@@ -0,0 +1,11 @@
+package model.data_access;
+
+public class LoginDaoImpl implements LoginDao{
+
+	@Override
+	public boolean checkLoginForm() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
